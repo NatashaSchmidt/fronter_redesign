0517c01fee1883f372fb66d15f69953839f7d1ea
Natasha Schmidt <natashaschmidt@desktop-9jp0ov7.kea.local>
2018-12-13 13:58:45 +0100

    færdig_prototype

--- script.js
+++ script.js
@@ -4,21 +4,40 @@ function sidenVises() {
     console.log("siden vises");
     //Hvad der skal ske
 }
+//
+//function visMateriale() {
+//    console.log("Vis Materiale");
+//    document.querySelector("#tema02_more").addEventListener("click", viserMateriale);
+//
+//
+//
+//}
+//
+//function viserMateriale() {
+//    document.querySelector("#tema02_more").classList.remove("hide");
+//    document.addEventListener("#tema02_more").classList.add("show");
+//    document.querySelector("#tema02_more").addEventListener("animationend", viserNuMateriale);
+//}
+//
+//function viserNuMateriale() {
+//    console.log("materiale vises");
+//}
 
-function visMateriale() {
-    console.log("Vis Materiale");
-    document.querySelector("#tema02_more").addEventListener("click", viserMateriale);
+function visMenu() {
+    console.log("vis menu");
 
-
+    var burgerknap = document.querySelector(".btn-menu");
+    var mobilmenu = document.querySelector("nav");
+    btn - menu.addEventListener("click", menuVises);
 
 }
 
-function viserMateriale() {
-    document.querySelector("#tema02_more").classList.remove("hide");
-    document.addEventListener("#tema02_more").classList.add("show");
-    document.querySelector("#tema02_more").addEventListener("animationend", viserNuMateriale);
-}
+function menuVises() {
+    console.log("menu er vist");
 
-function viserNuMateriale() {
-    console.log("materiale vises");
+    if (mobilmenu.style.display == "none" || mobilmenu.style.display == "") {
+        mobilmenu.style.display = "flex";
+    } else {
+        mobilmenu.style.display = "none";
+    }
 }
